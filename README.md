27a7610547b26077085b9dfaff10d26d42cc11e3
Jorge Macias <jorgemacias@Jorges-Mac-mini.local>
2019-11-11 11:20:21 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+### Log DVR serial
+
+* Acceder al archivo log.txt
+
+* Buscar la cadena que empieza con: 7e 9c
+
+Esta es la parte de log donde se hace el handshake:
+
+```
+2019/11/11 10:19:51 RX:
+00000000  20 28 65 6e 61 62 6c 65  29 20 45 6e 67 69 6e 65  | (enable) Engine|
+00000010  20 4f 4e 20 21 21 21 7e  9c 00 00 00 01 67 34 7e  | ON !!!~.....g4~|
+00000020  0d 0a                                             |..|
+
+
+2019/11/11 10:19:51 TX:
+00000000  7e 9c 00 00 00 01 67 34  7e 0d 0a                 |~.....g4~..|
+
+
+2019/11/11 10:19:52 RX:
+00000000  20 20 3d 3d 3d 3d 6e 6f  74 20 63 68 65 63 6b 20  |  ====not check |
+00000010  61 75 74 68 21 0d 0a                              |auth!..|
+```
+
+* La palabra not check auth quiere decir que ya hay un handshake
+
+* Si se vuelve a pedir el handshake hay que responder igual
+
+* Pero en mi experiencia una vez realizado ya no vuelve pedirlo
+
+
---
+++ serialparser.go
@@ -0,0 +1,119 @@
+// go get github.com/jacobsa/go-serial/serial
+package main
+
+import "fmt"
+import "log"
+import (
+        "github.com/jacobsa/go-serial/serial"
+        "encoding/hex"
+)
+
+
+func main() {
+        // Set up options.
+        options := serial.OpenOptions{
+                //PortName: "/dev/ttyS0",
+                PortName: "/dev/cu.usbserial-1430",
+                BaudRate: 115200,
+                DataBits: 8,
+                StopBits: 1,
+                MinimumReadSize: 4,
+        }
+
+        // Open the port.
+        port, err := serial.Open(options)
+        if err != nil {
+                log.Fatalf("serial.Open: %v", err)
+        }
+
+        // Make sure to close it later.
+        defer port.Close()
+
+        // HandShake Response
+        handshake          := []byte{0x7E,0x9c,0x00,0x00,0x00,0x01,0x67,0x34,0x7E}
+        querydoorstatus    := []byte{0x7E,0x9e,0x00,0x00,0x00,0x01,0x67,0x36,0x7E}
+        responsedoorstatus := []byte{0x7E,0x9e,0x00,0x00,0x00,0x01,0x67,0x36,0x00,0x00,0x7E,0x0d,0x0a}
+        passengercounterdata := []byte{0x7E, 0xA4, 0x00, 0x00, 0x00, 0x01, 0x67, 0x38, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x03, 0x7E,0x0d,0x0a}
+        sendhandshake      := []byte{0x7E,0x9c,0x00,0x00,0x00,0x01,0x67,0x34,0x7E,0x0d,0x0a}
+        cr := []byte{0x0d}
+        lf := []byte{0x0a}
+        buff := make([]byte, 1)
+        counter :=0;
+        parse := make([]byte, 0)
+        for {
+                n, err := port.Read(buff)
+                if err != nil {
+                        log.Fatal(err)
+                        break
+                }
+                if n == 0 {
+                        fmt.Println("\nEOF")
+                        break
+                }
+                if n > 0 {
+                        parse = append(parse,buff...)
+                        if (string(buff[:n])==string(cr)) {
+
+                                _, err := port.Read(buff)
+                                if err != nil {
+                                        log.Fatal(err)
+                                        break
+                                }
+                                parse = append(parse, buff...)
+                                if (string(buff[:n]) == string(lf)) {
+                                        log.Printf("RX:\n%v", hex.Dump(parse))
+                                        pointer := 0;
+                                        for _, x := range parse {
+                                                if x == handshake[pointer] {
+                                                        pointer = pointer + 1
+                                                        if pointer == 9 {
+                                                                sendhandshake[3] = byte(counter)
+                                                                _, err := port.Write(handshake)
+
+                                                                if err != nil {
+                                                                        log.Fatalf("port.Write: %v", err)
+                                                                } else {
+                                                                        log.Printf("TX:\n%v", hex.Dump(sendhandshake))
+                                                                }
+                                                                counter = counter + 1
+                                                                break
+                                                        }
+                                                }
+                                        }
+                                        pointer = 0;
+                                        for _, x := range parse {
+                                                if x == querydoorstatus[pointer] {
+                                                        pointer = pointer + 1
+                                                        if pointer == 9 {
+                                                                responsedoorstatus[3]=byte(counter)
+                                                                _, err := port.Write(responsedoorstatus)
+
+                                                                if err != nil {
+                                                                        log.Fatalf("port.Write: %v", err)
+                                                                } else {
+                                                                        log.Printf("TX:\n%v", hex.Dump(responsedoorstatus))
+                                                                }
+                                                                counter = counter + 1
+
+
+                                                                passengercounterdata[3]=byte(counter)
+                                                                _, err  = port.Write(passengercounterdata)
+
+                                                                if err != nil {
+                                                                        log.Fatalf("port.Write: %v", err)
+                                                                } else {
+                                                                        log.Printf("TX:\n%v", hex.Dump(passengercounterdata))
+                                                                }
+                                                                counter = counter + 1
+                                                                break
+                                                        }
+                                                }
+                                        }
+                                        parse = make([]byte, 0)
+                                }
+
+                        }
+                }
+
+        }
+}
